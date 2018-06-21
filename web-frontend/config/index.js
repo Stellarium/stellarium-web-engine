263719da73c329d4f160227e67abb1ae055804ba
Fabien Chereau <fabien.chereau@gmail.com>
2018-06-21 14:30:54 +0200

    Allow to specify a CDN when building static version
    To set the CDN, set the CDN_ENV environment variable while running
    the make build target.
    
    e.g.:
    CDN_ENV="https://xxx.cloudfront.net/" make build

--- web-frontend/config/index.js
+++ web-frontend/config/index.js
@@ -55,7 +55,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: process.env.CDN_ENV ? process.env.CDN_ENV : '/',
 
     /**
      * Source Maps
