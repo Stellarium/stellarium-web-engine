932aa162670046141ca20f928ffe4717201b13a1
Guillaume Chereau <guillaume@noctua-software.com>
2018-06-28 17:47:03 +0800

    Add SGP4 computation code from David Vallado
    The original code is in C++, so I also add a wrapper of the two
    functions so that we can use it from C.
    
    It is totally untested at the moment.

---
+++ src/sgp4.cpp
@@ -0,0 +1,55 @@
+/* Stellarium Web Engine - Copyright (c) 2018 - Noctua Software Ltd
+ *
+ * This program is licensed under the terms of the GNU AGPL v3, or
+ * alternatively under a commercial licence.
+ *
+ * The terms of the AGPL v3 license can be found in the main directory of this
+ * repository.
+ */
+
+
+/* Include the original SGP4.cpp file, with a lot of pragmas to prevent
+ * warnings!  */
+
+#ifdef __GNUC__
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wdangling-else"
+#pragma GCC diagnostic ignored "-Wunused-result"
+#pragma GCC diagnostic ignored "-Wunused-function"
+#pragma GCC diagnostic ignored "-Wformat"
+
+#ifndef __clang__
+#pragma GCC diagnostic ignored "-Wunused-but-set-variable"
+#pragma GCC diagnostic ignored "-Wmaybe-uninitialized"
+#endif
+
+#endif
+
+#include "../ext_src/sgp4/SGP4.cpp"
+
+#ifdef __GNUC__
+#pragma GCC diagnostic pop
+#endif
+
+extern "C" {
+    #include "sgp4.h"
+}
+
+sgp4_elsetrec_t *sgp4_twoline2rv(
+        const char str1_[130], const char str2_[130],
+        char typerun, char typeinput, char opsmode,
+        double *startmfe, double *stopmfe, double *deltamin)
+{
+    char str1[130], str2[130];
+    elsetrec *ret = (elsetrec*)calloc(1, sizeof(*ret));
+    strncpy(str1, str1_, 130);
+    strncpy(str2, str2_, 130);
+    SGP4Funcs::twoline2rv(str1, str2, typerun, typeinput, opsmode,
+                          wgs72, *startmfe, *stopmfe, *deltamin, *ret);
+    return (sgp4_elsetrec*)ret;
+}
+
+bool sgp4(sgp4_elsetrec_t *satrec, double tsince, double r[3], double v[3])
+{
+    return SGP4Funcs::sgp4(*((elsetrec*)satrec), tsince, r, v);
+}
---
+++ src/sgp4.h
@@ -0,0 +1,24 @@
+/* Stellarium Web Engine - Copyright (c) 2018 - Noctua Software Ltd
+ *
+ * This program is licensed under the terms of the GNU AGPL v3, or
+ * alternatively under a commercial licence.
+ *
+ * The terms of the AGPL v3 license can be found in the main directory of this
+ * repository.
+ */
+
+/*
+ * C bindings to the SGP4 functions for artifitial satellites position
+ * computation so that we can use them from C.
+ */
+
+#include <stdbool.h>
+
+typedef struct sgp4_elsetrec sgp4_elsetrec_t;
+
+sgp4_elsetrec_t *sgp4_twoline2rv(
+        const char str1[130], const char str2[130],
+        char typerun, char typeinput, char opsmode,
+        double *startmfe, double *stopmfe, double *deltamin);
+
+bool sgp4(sgp4_elsetrec_t *satrec, double tsince, double r[3], double v[3]);
