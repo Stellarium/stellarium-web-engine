043c879440227d44d5e9b449eec74c4d7b986141
Guillaume Chereau <guillaume@noctua-software.com>
2018-07-11 11:46:27 +0800

    Fix Saturn rings

--- src/modules/planets.c
+++ src/modules/planets.c
@@ -443,7 +443,7 @@ static void render_rings(texture_t *tex,
     painter.light_emit = NULL;
     painter.flags &= ~PAINTER_PLANET_SHADER;
     painter.flags |= PAINTER_NO_CULL_FACE;
-    paint_quad(&painter, FRAME_OBSERVED, tex, NULL, NULL, &proj, 64);
+    paint_quad(&painter, FRAME_VIEW, tex, NULL, NULL, &proj, 64);
 }
 
 /*
