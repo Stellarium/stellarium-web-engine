97fad7bac074996529caf2042fc4529666956496
Guillaume Chereau <guillaume@noctua-software.com>
2018-08-30 16:57:01 +0800

    Switch back dso survey url

--- src/modules/dso.c
+++ src/modules/dso.c
@@ -275,7 +275,7 @@ static int dsos_init(obj_t *obj, json_value *args)
     regcomp(&dsos->search_reg, "(m|ngc|ic|nsid) *([0-9]+)",
             REG_EXTENDED | REG_ICASE);
 
-    asprintf(&dsos->survey, "https://data.stellarium.org/surveys/dso2");
+    asprintf(&dsos->survey, "https://data.stellarium.org/surveys/dso");
     return 0;
 }
 
