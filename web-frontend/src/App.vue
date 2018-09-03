8911466a87d9457234a4589ab92623d76d8c9dae
Fabien Chereau <fabien.chereau@gmail.com>
2018-09-03 17:49:21 +0200

    Rely on emscripten code to load the WASM with streaming

--- web-frontend/src/App.vue
+++ web-frontend/src/App.vue
@@ -203,23 +203,17 @@ export default {
     NoctuaSkyClient.init(process.env.NOCTUASKY_API_SERVER, onNoctuaSkyStateChanged)
 
     import('@/assets/js/stellarium-web-engine.wasm').then(f => {
-      fetch(f).then(response => {
-        response.arrayBuffer().then(buffer => {
-          console.log('WASM fetched,length: ' + buffer.byteLength)
-
-          // Initialize the StelWebEngine viewer singleton
-          // After this call, the StelWebEngine state will always be available in vuex store
-          // in the $store.stel object in a reactive way (useful for vue components).
-          // To modify the state of the StelWebEngine, it's enough to call/set values directly on the $stel object
-          swh.initStelWebEngine(that.$store, buffer, that.$refs.stelCanvas, function () {
-            // Start auto location detection (even if we don't use it)
-            swh.getGeolocation(this).then(swh.geoCodePosition).then((loc) => {
-              that.$store.commit('setAutoDetectedLocation', loc)
-            }, (error) => { console.log(error) })
-            that.setStateFromQueryArgs()
-            that.guiComponent = 'Gui'
-          })
-        })
+      // Initialize the StelWebEngine viewer singleton
+      // After this call, the StelWebEngine state will always be available in vuex store
+      // in the $store.stel object in a reactive way (useful for vue components).
+      // To modify the state of the StelWebEngine, it's enough to call/set values directly on the $stel object
+      swh.initStelWebEngine(that.$store, f, that.$refs.stelCanvas, function () {
+        // Start auto location detection (even if we don't use it)
+        swh.getGeolocation(this).then(swh.geoCodePosition).then((loc) => {
+          that.$store.commit('setAutoDetectedLocation', loc)
+        }, (error) => { console.log(error) })
+        that.setStateFromQueryArgs()
+        that.guiComponent = 'Gui'
       })
     })
   }
