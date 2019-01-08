b5148f839d692c213b051f3df77cc83e8702ee85
Fabien Chereau <fabien.chereau@gmail.com>
2019-01-08 12:47:18 +0100

    Refactored pointer display
    CHange style, and kakes use of new obj_get_2d_ellipse function.

--- src/modules/pointer.c
+++ src/modules/pointer.c
@@ -21,54 +21,34 @@ typedef struct pointer {
 static int pointer_render(const obj_t *obj, const painter_t *painter_)
 {
     int i;
-    double pos[4], pos2d[2];
-    double size, luminance, radius = NAN;
-    double white[4] = {1, 1, 1, 1};
+    double win_pos[2], win_size[2], angle;
     const double T = 2.0;    // Animation period.
     double r, transf[3][3];
     obj_t *selection = core->selection;
     painter_t painter = *painter_;
-    vec4_set(painter.color, 0.34, 0.59, 1.0, 0.9);
+    vec4_set(painter.color, 1, 1, 1, 1);
     if (!selection) return 0;
 
     // If the selection has a custom rendering method, we use it.
     if (selection->klass->render_pointer) {
-        if (selection->klass->render_pointer(selection, &painter) == 0)
-            return 0;
+        selection->klass->render_pointer(selection, &painter);
     }
 
-    obj_get_pos_observed(selection, painter.obs, pos);
-    mat3_mul_vec3(painter.obs->ro2v, pos, pos);
-    if (!project(painter.proj, PROJ_TO_WINDOW_SPACE, 2, pos, pos2d))
-        return 0;
-
-    // Empirical formula to compute the pointer size.
-    core_get_point_for_mag(selection->vmag, &size, &luminance);
-    size = core_get_apparent_angle_for_point(painter.proj, size);
-    if (obj_has_attr(selection, "radius")) {
-        obj_get_attr(selection, "radius", "f", &radius);
-        size = max(size, radius);
-    }
-
-    if (isnan(radius)) {
-        size = 4000.0 * size / core->fov;
-        size = max(size, 32);
-        size *= 1 + 0.05 * (sin(sys_get_unix_time() / T * 2 * M_PI) + 1);
-        symbols_paint(&painter, SYMBOL_POINTER, pos2d, VEC(size, size),
-                      white, 0);
-    } else {
-        // Draw four strokes around the object.
-        for (i = 0; i < 4; i++) {
-            r = size / core->fov * core->win_size[0] + 5;
-            r = max(r, 8);
-            r += 0.8 * (sin(sys_get_unix_time() / T * 2 * M_PI) + 1);
-            mat3_set_identity(transf);
-            mat3_itranslate(transf, pos2d[0], pos2d[1]);
-            mat3_rz(i * 90 * DD2R, transf, transf);
-            mat3_itranslate(transf, r, 0);
-            mat3_iscale(transf, 8, 1, 1);
-            paint_2d_line(&painter, transf, VEC(0, 0), VEC(1, 0));
-        }
+    obj_get_2d_ellipse(selection, &painter, win_pos, win_size,
+                       &angle);
+    r = max(win_size[0], win_size[1]);
+    r += 5;
+    // Draw four strokes around the object.
+    for (i = 0; i < 4; i++) {
+        r = max(r, 8);
+        r += 0.4 * (sin(sys_get_unix_time() / T * 2 * M_PI) + 1.1);
+        mat3_set_identity(transf);
+        mat3_itranslate(transf, win_pos[0], win_pos[1]);
+        mat3_rz(i * 90 * DD2R, transf, transf);
+        mat3_itranslate(transf, r, 0);
+        mat3_iscale(transf, 8, 1, 1);
+        painter.lines_width = 3;
+        paint_2d_line(&painter, transf, VEC(0, 0), VEC(1, 0));
     }
     return 0;
 }
