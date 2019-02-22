85cb2c15e832c737afa0fb0db2aa813e20d463fc
Fabien Chereau <fabien.chereau@gmail.com>
2019-02-22 11:10:45 +0100

    Made cardinal labels more visible especially in daylight

--- src/modules/cardinal.c
+++ src/modules/cardinal.c
@@ -40,7 +40,26 @@ static int cardinal_render(const obj_t *obj, const painter_t *painter)
 {
     int i;
     double size = 24;
-    double color[4] = {0.5, 0.4, 0.4, 0.5};
+    obj_t *sun;
+    double pos[4];
+    double sin_angle;
+    double brightness = 0.0;
+    double color[4];
+
+    // Compute global brightness to adjust opacity/color of labels
+    sun = obj_get_by_oid(&core->obj, oid_create("HORI", 10), 0);
+    obj_get_pos_observed(sun, core->observer, pos);
+    vec3_normalize(pos, pos);
+    sin_angle = sin(min(M_PI/ 2, asin(pos[2]) + 8. * DD2R));
+    if (sin_angle > -0.1 / 1.5 )
+        brightness += 1.5 * (sin_angle + 0.1 / 1.5);
+    brightness = min(brightness, 1.0);
+
+    color[0] = 0.6 + 0.4 * brightness;
+    color[1] = 0.5 - 0.3 * brightness;
+    color[2] = 0.5 - 0.3 * brightness;
+    color[3] = 0.6 + 0.4 * brightness;
+
     for (i = 0; i < ARRAY_SIZE(POINTS); i++) {
         if (painter_is_point_clipped_fast(painter, FRAME_OBSERVED,
                 POINTS[i].pos, true))
