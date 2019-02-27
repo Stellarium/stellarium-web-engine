04762c30b2f37ddf8c074f2da8d4de6cd30df484
Guillaume Chereau <guillaume@noctua-software.com>
2019-02-27 14:14:08 +0800

    Fix saturn rings rendering
    The previous fix of using the middle quad pos for clipping wasn't
    enough.  Here we force at least one split of the quad.

--- src/traverse.c
+++ src/traverse.c
@@ -81,7 +81,7 @@ static int on_node(qtree_node_t *node, void *user)
 {
     d_t *d = user;
     double mat[3][3], uv[4][2];
-    double pos[4][4], clip[5][4];
+    double pos[4][4], clip[4][4];
     double mid_pos[4], sep = 0;
     int i, r;
 
@@ -112,13 +112,17 @@ static int on_node(qtree_node_t *node, void *user)
     mat4_mul_vec4(*d->painter->transform, mid_pos, mid_pos);
     convert_framev4(d->painter->obs, d->frame, FRAME_VIEW,
                         mid_pos, mid_pos);
-    // Add the middle pos in the clip test points.
-    project(d->painter->proj, 0, 4, mid_pos, clip[4]);
     sep = eraSepp(mid_pos, pos[0]) * 2;
+
+    if (sep < M_PI) goto skip_clipping_test;
+    // Special fix for ring textures: need to split at least once.
+    if (d->painter->flags & PAINTER_RING_SHADER)
+        if (node->level < 1) goto skip_clipping_test;
 
     // For large angles, we just go down.  I think we could optimize this if
     // needed.
-    if (sep < M_PI && is_clipped(5, clip)) return 0;
+    if (is_clipped(4, clip)) return 0;
+skip_clipping_test:
 
     r = d->f(1, node, d->uv, pos, mat, d->painter, d->user);
     if (r != 2) return r;
