Stellarium/stellarium-web-engine

commit 4fceffdbb9ea45c392888f8e06fbbee7f225162e

Make eye adaptation speed a bit slower and not depend on FPS.

src/core.c
@@ -405,7 +405,8 @@ int core_update(double dt)
     // Update eye adaptation.
     lwmax = core->lwmax * core->lwmax_scale;
     lwmax = exp(logf(core->tonemapper.lwmax) +
-                (logf(lwmax) - logf(core->tonemapper.lwmax)) * 0.1);
+                (logf(lwmax) - logf(core->tonemapper.lwmax)) *
+                0.05 * dt / 0.01666);
     tonemapper_update(&core->tonemapper, -1, -1, -1, lwmax);
     core->lwmax = core->lwmax_min; // Reset for next frame.
 

commit bf031a5e802822976a22b0b64c0768940d1ee756

Fix almost freeze at startup on slow CPU
It's a regression from commit 4fceffdb
When dt is verly large (like 1s) at startup on slow CPUs, lwmax
is computed to zero causing the core to render all stars
at 120 deg zoom level.

src/core.c
@@ -406,7 +406,7 @@ int core_update(double dt)
     lwmax = core->lwmax * core->lwmax_scale;
     lwmax = exp(logf(core->tonemapper.lwmax) +
                 (logf(lwmax) - logf(core->tonemapper.lwmax)) *
-                0.05 * dt / 0.01666);
+                min(0.05 * dt / 0.01666, 0.5));
     tonemapper_update(&core->tonemapper, -1, -1, -1, lwmax);
     core->lwmax = core->lwmax_min; // Reset for next frame.
 