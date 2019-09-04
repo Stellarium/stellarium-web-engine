e05039ba2235988b644b169ff5ed7b7dc68c809f
Fabien Chereau <fabien.chereau@gmail.com>
2019-09-04 02:11:37 +0200

    Re-adjust ad-hoc milkyway brightness

--- src/modules/milkyway.c
+++ src/modules/milkyway.c
@@ -52,7 +52,7 @@ static int milkyway_render(const obj_t *obj, const painter_t *painter_)
 
     // Modulate luminance from atmosphere average brightness so that
     // milky way becomes less visible with a full moon
-    c *= min(0.0002 / max(0.000001, core->lwsky_average), 1.0);
+    c *= min(0.004 / max(0.000001, core->lwsky_average), 1.0);
     c = max(c, 0) * 0.7;
     painter.color[3] *= c;
 
