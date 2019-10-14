eddf00e6255ec57f4058d41f75f6606c556dabc0
Guillaume Chereau <guillaume@noctua-software.com>
2019-10-14 14:31:44 +0800

    Grids: don't skip lines around discontinuities
    Mostly to improve the grid with the Mollweide projection.

--- src/modules/lines.c
+++ src/modules/lines.c
@@ -406,8 +406,7 @@ static void render_recursion(
         if (done_mask & (1 << dir)) continue; // Marked as done already.
         if (splits[dir] != steps[dir]->n / (dir ? 2 : 1)) continue;
         done_mask |= (1 << dir);
-        paint_lines(painter, line->frame, 2, lines + dir * 2,
-                    &map, 8, PAINTER_SKIP_DISCONTINUOUS);
+        paint_lines(painter, line->frame, 2, lines + dir * 2, &map, 8, 0);
         if (!line->format) continue;
         if (check_borders(pos[0], pos[2 - dir], painter->proj, p, u, v)) {
             render_label(p, u, v, uv[0], 1 - dir, line,
