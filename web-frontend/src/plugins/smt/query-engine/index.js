0e82148e21734c7fc05147079ea0d1dc4ced06c1
Fabien Chereau <fabien.chereau@gmail.com>
2019-10-17 01:51:40 +0200

    Made webworker also work in devlopement mode

--- web-frontend/src/plugins/smt/query-engine/index.js
+++ web-frontend/src/plugins/smt/query-engine/index.js
@@ -3,8 +3,13 @@
 // All rights reserved
 
 import PromiseWorker from 'promise-worker'
+
+// The line below is the best I could find to get webworker running in dev mode
+// as for some reasons, the inline version won't run on my localhost and the not
+// inline version doesn't run on the real server because of CORS settings..
+
 // eslint-disable-next-line
-import Worker from 'worker-loader?{"inline":true,"fallback":false}!./worker'
+const Worker = process.env.NODE_ENV === 'development' ? require('worker-loader!./worker') : require('worker-loader?{"inline":true,"fallback":false}!./worker')
 
 const promiseWorker = new PromiseWorker(new Worker())
 
