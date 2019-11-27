42b01ac6837d38c29104b03e50feb7f6780b542c
Guillaume Chereau <guillaume@noctua-software.com>
2019-11-27 20:42:54 +0800

    Add missing values to cached hip positions
    Some important stars are missing from Hipparcos New Reduction data set!
    For example HIP 78727 (Xi Scorpii).  I add them by using the Hipparcos
    original dataset as fallback.
    
    This only add them to the HIP -> Healpix mapping for fast search.  The
    stars data are still missing, but at least now we quickly return NULL
    when searching for those stars.
    
    We should probably fix the data as well somehow.

--- tools/make-hip.py
+++ tools/make-hip.py
@@ -21,6 +21,35 @@ import sys
 sys.path.append('./tools')
 from utils import generator
 
+def hms_to_deg(hms):
+    if hms.startswith("b'"): hms = hms[2:-2] # For gaia python3 bug.
+    hms = [float(x) for x in hms.split()]
+    return hms[0] * 15 + hms[1] * 15 / 60 + hms[2] * 15 / 60 / 60
+
+def dms_to_deg(dms):
+    if dms.startswith("b'"): dms = dms[2:-2] # For gaia python3 bug.
+    dms = [float(x) for x in dms.split()]
+    return dms[0] + dms[1] / 60 + dms[2] / 60 / 60
+
+@generator('hip.csv', md5='f2f11a8f3f8ccae13f6eabf331b4e4db')
+def get_hip_data_file(path):
+    '''Get HIP data from gaia TAP server'''
+    # We use the hipparcos new reduction table, as it is supposed to have
+    # better values.
+    from astroquery.gaia import Gaia
+    QUERY = '''
+        SELECT hip, ra, de, vmag, rahms, dedms
+        FROM public.hipparcos
+        ORDER BY hip
+    '''
+    job = Gaia.launch_job_async(QUERY)
+    r = job.get_results()
+    r.write(path, overwrite=True, format='ascii.csv')
+    if not hasattr(job, 'get_jobid'):
+        print('Warning: cannot delete Gaia job')
+        return
+    Gaia.remove_jobs([job.get_jobid()])
+
 @generator('hip_newreduction.csv', md5='b0b86439386bbcbae15efc402ed06da7')
 def get_hip_newreduction_data_file(path):
     '''Get HIP data from gaia TAP server'''
@@ -41,14 +70,27 @@ def get_hip_newreduction_data_file(path):
     Gaia.remove_jobs([job.get_jobid()])
 
 def run():
-    data = list(csv.DictReader(open(get_hip_newreduction_data_file())))
     htable = {}
 
+    data = list(csv.DictReader(open(get_hip_newreduction_data_file())))
     for d in data:
+        hip = int(d['hip'])
         ra = float(d['ra'])
         de = float(d['dec'])
         pix = healpy.ang2pix(4, ra, de, nest=True, lonlat=True)
-        htable[int(d['hip'])] = pix
+        htable[hip] = pix
+
+    # Add the stars missing from Hipparcos New Reduction!
+    # XXX: would be nice to be able to load them all in a single SQL query.
+    data = list(csv.DictReader(open(get_hip_data_file())))
+    for d in data:
+        hip = int(d['hip'])
+        if hip in htable: continue
+        ra = hms_to_deg(d['rahms'])
+        de = dms_to_deg(d['dedms'])
+        pix = healpy.ang2pix(4, ra, de, nest=True, lonlat=True)
+        htable[hip] = pix
+
     data = [htable.get(x, 255) for x in range(max(htable.keys()))]
 
     out = open('src/hip.inl', 'w')
