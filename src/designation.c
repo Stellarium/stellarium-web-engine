6e6708335dfc1139b8c5cda2934caec57254661c
Fabien Chereau <fabien.chereau@gmail.com>
2019-12-13 15:24:57 +0100

    Add support for exponent like in Psi5 Aqu

--- src/designation.c
+++ src/designation.c
@@ -141,15 +141,19 @@ static const char *CSTS[88][2] = {
  *   dsgn   - A designation (eg: '* alf Aqr')
  *   cst    - Output constellation id
  *   bayer  - Output bayer number (1 -> α, 2 -> β, etc.)
+ *   nb     - Output extra number to use as exponent. 0 means no exponent.
  *
  * Return:
  *   False if the designation doesn't match a bayer name.
  */
-static bool designation_parse_bayer(const char *dsgn, int *cst, int *bayer)
+static bool designation_parse_bayer(const char *dsgn, int *cst, int *bayer,
+                                    int *nb)
 {
     int i;
+    char *endptr;
 
     *bayer = 0;
+    *nb = 0;
 
     if (!dsgn) return false;
     // Parse the '* '
@@ -165,6 +169,11 @@ static bool designation_parse_bayer(const char *dsgn, int *cst, int *bayer)
     if (i == 25) return false;
     dsgn += strlen(GREEK[i][1]);
     if (*dsgn == '.') dsgn++;
+
+    *nb = strtol(dsgn, &endptr, 10);
+    if (*nb != 0)
+        dsgn = endptr;
+
     if (*dsgn == ' ') dsgn++;
 
     *bayer = i + 1;
@@ -220,6 +229,32 @@ static bool designation_parse_flamsteed(const char *dsgn, int *cst,
     return true;
 }
 
+static const char * to_exponent(char c) {
+    switch (c) {
+    case 48:
+        return "⁰";
+    case 49:
+        return "¹";
+    case 50:
+        return "²";
+    case 51:
+        return "³";
+    case 52:
+        return "⁴";
+    case 53:
+        return "⁵";
+    case 54:
+        return "⁶";
+    case 55:
+        return "⁷";
+    case 56:
+        return "⁸";
+    case 57:
+        return "⁹";
+    };
+    return "";
+}
+
 /*
  * Function: designation_cleanup
  * Create a printable version of a designation
@@ -229,30 +264,40 @@ static bool designation_parse_flamsteed(const char *dsgn, int *cst,
 void designation_cleanup(const char *dsgn, char *out, int size, int flags)
 {
     int cst;
-    int i, n;
+    int i, g, nb;
     const char *remove[] = {"NAME ", "* ", "Cl ", "Cl* ", "** ", "MPC "};
     const char *greek;
     const char *cstname;
+    char tmp[64];
+    char exponent[256];
 
     if (strncmp(dsgn, "V* ", 3) == 0)
         dsgn++;
 
-    if (designation_parse_bayer(dsgn, &cst, &n)) {
-        greek = (flags & BAYER_LATIN_SHORT) ? GREEK[n - 1][2] :
-                (flags & BAYER_LATIN_LONG) ? GREEK[n - 1][3] : GREEK[n - 1][0];
+    if (designation_parse_bayer(dsgn, &cst, &g, &nb)) {
+        exponent[0] = 0;
+        tmp[0] = 0;
+        greek = (flags & BAYER_LATIN_SHORT) ? GREEK[g - 1][2] :
+                (flags & BAYER_LATIN_LONG) ? GREEK[g - 1][3] : GREEK[g - 1][0];
+        if (nb) {
+            snprintf(tmp, sizeof(tmp), "%d", nb);
+            for (i = 0; i < strlen(tmp); ++i)
+                strncat(exponent, to_exponent(tmp[i]),
+                        sizeof(exponent) - strlen(exponent) - 1);
+        }
         if (flags & BAYER_CONST_SHORT || flags & BAYER_CONST_LONG) {
             cstname = (flags & BAYER_CONST_SHORT) ? CSTS[cst][0] : CSTS[cst][1];
-            snprintf(out, size, "%s %s", greek, cstname);
+            snprintf(out, size, "%s%s %s", greek, exponent, cstname);
         } else
-            snprintf(out, size, "%s", greek);
+            snprintf(out, size, "%s%s", greek, exponent);
         return;
     }
-    if (designation_parse_flamsteed(dsgn, &cst, &n)) {
+    if (designation_parse_flamsteed(dsgn, &cst, &g)) {
         if (flags & BAYER_CONST_SHORT || flags & BAYER_CONST_LONG) {
             cstname = (flags & BAYER_CONST_SHORT) ? CSTS[cst][0] : CSTS[cst][1];
-            snprintf(out, size, "%d %s", n, cstname);
+            snprintf(out, size, "%d %s", g, cstname);
         } else
-            snprintf(out, size, "%d", n);
+            snprintf(out, size, "%d", g);
         return;
     }
 
@@ -271,10 +316,10 @@ void designation_cleanup(const char *dsgn, char *out, int size, int flags)
 static void test_designations(void)
 {
     char buf[128];
-    int n, cst;
+    int n, cst, nb;
     bool r;
 
-    r = designation_parse_bayer("* alf Aqr", &cst, &n);
+    r = designation_parse_bayer("* alf Aqr", &cst, &n, &nb);
     assert(r && strcmp(CSTS[cst][0], "Aqr") == 0 && n == 1);
     r = designation_parse_flamsteed("* 10 Aqr", &cst, &n);
     assert(r && strcmp(CSTS[cst][0], "Aqr") == 0 && n == 10);
@@ -283,6 +328,12 @@ static void test_designations(void)
     assert(strcmp(buf, "Polaris") == 0);
     designation_cleanup("* alf Aqr", buf, sizeof(buf), 0);
     assert(strcmp(buf, "α") == 0);
+    designation_cleanup("* alf1 Aqr", buf, sizeof(buf), 0);
+    assert(strcmp(buf, "α¹") == 0);
+    designation_cleanup("* alf0123456789 Aqr", buf, sizeof(buf), 0);
+    assert(strcmp(buf, "α¹²³⁴⁵⁶⁷⁸⁹") == 0);
+    designation_cleanup("* alf04 Aqr", buf, sizeof(buf), 0);
+    assert(strcmp(buf, "α⁴") == 0);
     designation_cleanup("* alf Aqr", buf, sizeof(buf), BAYER_LATIN_SHORT);
     assert(strcmp(buf, "Alf") == 0);
     designation_cleanup("* alf Aqr", buf, sizeof(buf), BAYER_LATIN_LONG);
