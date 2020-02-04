65ce041a0de03a1dd2fa8a1d8d113e7399838d63
Fabien Chereau <fabien.chereau@gmail.com>
2020-02-04 17:19:57 +0100

    Minor style change
    Done by vue-cli tool

--- web-frontend/.postcssrc.js
+++ web-frontend/.postcssrc.js
@@ -1,9 +1,9 @@
 // https://github.com/michael-ciniawsky/postcss-load-config
 
 module.exports = {
-  "plugins": {
+  'plugins': {
     // to edit target browsers: use "browserslist" field in package.json
-    "postcss-import": {},
-    "autoprefixer": {}
+    'postcss-import': {},
+    'autoprefixer': {}
   }
 }
