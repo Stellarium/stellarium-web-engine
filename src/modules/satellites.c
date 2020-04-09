e51caa0b95fa836136aeaed2aeae1389f26b44b0
Guillaume Chereau <guillaume@noctua-software.com>
2020-04-09 14:24:32 +0800

    Satellites: don't list satellites with errors
    Because they trigger errors if we try to compute their position.

--- src/modules/satellites.c
+++ src/modules/satellites.c
@@ -456,6 +456,27 @@ static json_value *satellite_data_fn(obj_t *obj, const attribute_t *attr,
     return NULL;
 }
 
+static int satellites_list(const obj_t *obj, observer_t *obs,
+                           double max_mag, uint64_t hint,
+                           const char *sources, void *user,
+                           int (*f)(void *user, obj_t *obj))
+{
+    obj_t *child;
+    satellite_t *sat;
+    double vmag;
+    bool test_vmag = !isnan(max_mag);
+
+    DL_FOREACH(obj->children, child) {
+        sat = (void*)child;
+        if (sat->error) continue;
+        if (test_vmag && obj_get_info(child, obs, INFO_VMAG, &vmag) == 0 &&
+                vmag > max_mag)
+            continue;
+        if (f && f(user, child)) break;
+    }
+    return 0;
+}
+
 /*
  * Meta class declarations.
  */
@@ -487,6 +508,7 @@ static obj_klass_t satellites_klass = {
     .update         = satellites_update,
     .render         = satellites_render,
     .get_by_oid     = satellites_get_by_oid,
+    .list           = satellites_list,
     .attributes = (attribute_t[]) {
         PROPERTY(visible, TYPE_BOOL, MEMBER(satellites_t, visible)),
         PROPERTY(hints_mag_offset, TYPE_FLOAT,
