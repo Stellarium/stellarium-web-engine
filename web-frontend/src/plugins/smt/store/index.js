76647d6566906a4ad4a579a7696b3d2772c0af99
Fabien Chereau <fabien.chereau@gmail.com>
2020-09-09 14:24:48 +0300

    Update licence information before moving the code public

--- web-frontend/src/plugins/smt/store/index.js
+++ web-frontend/src/plugins/smt/store/index.js
@@ -1,6 +1,13 @@
-// Survey Monitoring Tool
-// Copyright (c) 2019 - Stellarium Labs SAS
-// All rights reserved
+// Stellarium Web - Copyright (c) 2020 - Stellarium Labs SAS
+//
+// This program is licensed under the terms of the GNU AGPL v3, or
+// alternatively under a commercial licence.
+//
+// The terms of the AGPL v3 license can be found in the main directory of this
+// repository.
+//
+// This file is part of the Survey Monitoring Tool plugin, which received
+// funding from the Centre national d'études spatiales (CNES).
 
 import _ from 'lodash'
 
