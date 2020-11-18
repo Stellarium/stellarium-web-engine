a7cf8baab3814553da9e7f6b5d28b4f94fd068a6
Fabien Chereau <fabien.chereau@gmail.com>
2020-11-18 19:19:38 +0200

    smt-server: split all footprints on Healpix boundaries
    If an observation footprint overlaps 2 healpix pixels, it is now
    cut into pieces, each being independent items, but sharing the same id.

--- smt-server/query-engine.mjs
+++ smt-server/query-engine.mjs
@@ -16,10 +16,111 @@ import hash_sum from 'hash-sum'
 import healpix from '@hscmap/healpix'
 import turf from '@turf/turf'
 import assert from 'assert'
+import glMatrix from 'gl-matrix'
+
+// Use native JS array in glMatrix lib
+glMatrix.glMatrix.setMatrixArrayType(Array)
 
 const D2R = Math.PI / 180
 const R2D = 180 / Math.PI
 const HEALPIX_ORDER = 1
+
+const crossAntimeridian = function (feature) {
+  let left = false
+  let right = false
+
+  turf.coordEach(feature, function(coord) {
+    if (left && right) return
+    let lng = coord[0]
+    if (lng > 180) lng -= 360
+    right = right || (lng >= 90)
+    left = left || (lng <= -90)
+  })
+  return left && right
+}
+
+const normalizeGeoJson = function (geoData) {
+  // Normalize coordinates between -180;180
+  turf.coordEach(geoData, function(coord) {
+    if (coord[0] > 180) coord[0] -= 360
+  })
+
+  // Shift polygons crossing antimeridian
+  turf.geomEach(geoData, function(geom) {
+    if (!crossAntimeridian(geom)) return
+    turf.coordEach(geom, function(coord) {
+      if (coord[0] < 0) coord[0] += 360
+    })
+  })
+}
+
+const geojsonPointToVec3 = function (p) {
+  const theta = (90 - p[1]) * D2R
+  const phi = (p[0] < 0 ? p[0] + 360 : p[0]) * D2R
+  assert((theta >= 0) && (theta <= Math.PI))
+  assert(phi >= 0 && phi <= 2 * Math.PI)
+  return healpix.ang2vec(theta, phi)
+}
+
+const vec3TogeojsonPoint = function (v) {
+  const p = healpix.vec2ang(v)
+  let lat = 90 - p.theta * R2D
+  let lon = p.phi * R2D
+  if (lon > 180) lon -= 360
+  if (lon < -180) lon += 360
+  assert((lat >= -90) && (lat <= 90))
+  assert((lon >= -180) && (lon <= 180))
+  return [lon, lat]
+}
+
+const rotateGeojsonPoint = function (p, m) {
+  const v = geojsonPointToVec3(p)
+  glMatrix.vec3.transformMat3(v, v, m)
+  const p2 = vec3TogeojsonPoint(v)
+  p[0] = p2[0]
+  p[1] = p2[1]
+}
+
+const rotateGeojsonFeature = function (feature, m) {
+  turf.coordEach(feature, p => {rotateGeojsonPoint(p, m)})
+}
+
+const intersectionRobust = function (feature1, feature2, shiftCenter) {
+  let q = glMatrix.quat.create()
+  const center = geojsonPointToVec3(shiftCenter)
+  glMatrix.quat.rotationTo(q, glMatrix.vec3.fromValues(center[0], center[1], center[2]), glMatrix.vec3.fromValues(1, 0, 0))
+  const m = glMatrix.mat3.create()
+  const mInv = glMatrix.mat3.create()
+  glMatrix.mat3.fromQuat(m, q)
+  glMatrix.mat3.invert(mInv, m)
+
+  const f1 = _.cloneDeep(feature1)
+  const f2 = _.cloneDeep(feature2)
+  rotateGeojsonFeature(f1, m)
+  rotateGeojsonFeature(f2, m)
+  let res = null
+  try {
+    if (f1.geometry.type === 'MultiPolygon') {
+      const intPoly = []
+      for (let i = 0; i < f1.geometry.coordinates.length; ++i) {
+          const poly = turf.polygon(f1.geometry.coordinates[i])
+          const intersection = turf.intersect(poly, f2)
+          if (intersection && intersection.geometry.type === 'Polygon') intPoly.push(intersection.geometry.coordinates)
+      }
+      if (intPoly.length) {
+        f1.geometry.coordinates = intPoly
+        res = f1
+      }
+    } else {
+      res = turf.intersect(f1, f2)
+    }
+    if (res) rotateGeojsonFeature(res, mInv)
+  } catch (err) {
+    console.log('Error computing feature intersection: ' + err)
+    res = null
+  }
+  return res
+}
 
 export default {
   fieldsList: undefined,
@@ -88,11 +189,7 @@ export default {
     }
   },
 
-  computeHealpixIndex: function (feature, order) {
-    let area = turf.area(feature) * 4 * Math.PI / 509600000 / (1000 * 1000)
-    // For large footprints, we return -1 so that it goes in the AllSky order
-    if (area > healpix.nside2pixarea(1 << order))
-      return -1
+  computeCentroidHealpixIndex: function (feature, order) {
     let center = turf.centroid(feature)
     assert(center)
     center = center.geometry.coordinates
@@ -104,43 +201,85 @@ export default {
     return healpix.ang2pix_nest(1 << order, theta, phi)
   },
 
+  getHealpixCornerFeature: function (order, pix) {
+    const mod = function(v, n) {
+      return ( v + n ) % n
+    }
+    let corners = healpix.corners_nest(1 << order, pix)
+    corners = corners.map(c => { return vec3TogeojsonPoint(c) })
+    for (let i = 0; i < corners.length; ++i) {
+      if (Math.abs(corners[i][1]) > 89.9999999) {
+        corners[i][0] = corners[mod(i + 1, corners.length)][0]
+        corners.splice(i, 0, [corners[mod(i - 1, corners.length)][0], corners[i][1]])
+        break
+      }
+    }
+    corners.push(_.cloneDeep(corners[0]))
+    let hppixel = turf.polygon([corners])
+    normalizeGeoJson(hppixel)
+    return hppixel
+  },
+
+  // Split the given feature in pieces on the healpix grid
+  // Returns a list of features each with 'healpix_index' set to the matching
+  // index.
+  splitOnHealpixGrid: function (feature, order) {
+    const that = this
+    let area = turf.area(feature) * 4 * Math.PI / 509600000 / (1000 * 1000)
+    // For large footprints, we return -1 so that it goes in the AllSky order
+    if (area > healpix.nside2pixarea(1 << order)) {
+      feature['healpix_index'] = -1
+      return [feature]
+    }
+    let center = turf.centroid(feature)
+    assert(center)
+    center = center.geometry.coordinates
+    let radius = 0
+    turf.coordEach(feature, (coord) => {
+      const d = turf.distance(coord, center, {units: 'radians'})
+      if (d > radius) radius = d
+    })
+
+    const v = geojsonPointToVec3(center)
+    const ret = []
+    //console.log('-----------------')
+    healpix.query_disc_inclusive_nest(1 << order, v, radius, function (pix) {
+      let hppixel = that.getHealpixCornerFeature(order, pix)
+      //console.log(JSON.stringify(hppixel) + ',')
+      const intersection = intersectionRobust(feature, hppixel, center)
+      if (intersection === null) return
+      const f = _.cloneDeep(feature)
+      f['healpix_index'] = pix
+      f.geometry = intersection.geometry
+      ret.push(f)
+    })
+    if (ret.length === 0) {
+      console.log(JSON.stringify(feature))
+      //process.exit()
+    }
+    return ret
+  },
+
   loadAllData: async function (jsonData) {
     console.log('Loading ' + jsonData.features.length + ' features')
     let that = this
 
-    // Normalize coordinates between -180;180
-    turf.coordEach(jsonData, function(coord) {
-      if (coord[0] > 180) coord[0] -= 360
-    })
+//    for (let pix=0; pix < 12 * (2 << HEALPIX_ORDER); pix++) {
+//      let hppixel = that.getHealpixCornerFeature(HEALPIX_ORDER, pix)
+//      console.log(JSON.stringify(hppixel) + ',')
+//    }
+//    process.exit()
 
-    function crossAntimeridian(feature) {
-      let left = false
-      let right = false
-
-      turf.coordEach(feature, function(coord) {
-        if (left && right) return
-        let lng = coord[0]
-        if (lng > 180) lng -= 360
-        right = right || (lng >= 90)
-        left = left || (lng <= -90)
-      })
-      return left && right
-    }
-
-    console.log('Shift polygons crossing antimeridian')
-    turf.geomEach(jsonData, function(geom) {
-      if (!crossAntimeridian(geom)) return
-      turf.coordEach(geom, function(coord) {
-        if (coord[0] < 0) coord[0] += 360
-      })
-    })
+    normalizeGeoJson(jsonData)
 
     // Insert all data
-    for (let feature of jsonData.features) {
-      const healpix_index = that.computeHealpixIndex(feature, HEALPIX_ORDER)
-      feature['healpix_index'] = healpix_index
+    let subFeatures = []
+    turf.featureEach(jsonData, function (feature, featureIndex) {
       feature['geogroup_id'] = _.get(feature.properties, 'SurveyName', '') + _.get(feature, 'id', '')
-
+      feature.id = that.fcounter++
+      subFeatures = subFeatures.concat(that.splitOnHealpixGrid(feature, HEALPIX_ORDER))
+    })
+    for (let feature of subFeatures) {
       for (let i = 0; i < that.fieldsList.length; ++i) {
         const field = that.fieldsList[i]
         let d
@@ -155,9 +294,8 @@ export default {
         }
         feature[that.sqlFields[i]] = d
       }
-      feature.id = that.fcounter++
     }
-    await alasql.promise('SELECT * INTO features FROM ?', [jsonData.features])
+    await alasql.promise('SELECT * INTO features FROM ?', [subFeatures])
   },
 
   loadGeojson: function (url) {
