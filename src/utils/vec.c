be6f6bf92f61be43e7e96d04598e0fb35c920718
Guillaume Chereau <guillaume@noctua-software.com>
2021-04-17 14:47:04 +0800

    vec.h: make some of the functions non inline
    No need to put everything inline.  We compile with link time
    optimisation anyway.

--- src/utils/vec.c
+++ src/utils/vec.c
@@ -15,6 +15,8 @@
     #include "vec.h"
 #endif
 
+#define S static
+
 static double min(double x, double y)
 {
     return x < y ? x : y;
@@ -101,6 +103,236 @@ void quat_rotate_towards(const double a[4], const double b[4],
     t = min(1.0, max_angle / sep);
     quat_slerp(a, b, t, out);
 }
+
+bool mat3_invert(const double mat[S 3][3], double out[S 3][3])
+{
+    double det;
+    int i;
+    const double *m = (const double *)mat;
+    double inv[9];
+
+#define M(i, j) m[i] * m[j]
+    inv[0] =   M(4, 8) - M(7, 5);
+    inv[3] =  -M(3, 8) + M(6, 5);
+    inv[6] =   M(3, 7) - M(6, 4);
+
+    inv[1] =  -M(1, 8) + M(7, 2);
+    inv[4] =   M(0, 8) - M(6, 2);
+    inv[7] =  -M(0, 7) + M(6, 1);
+
+    inv[2] =   M(1, 5)  - M(4, 2);
+    inv[5] =  -M(0, 5)  + M(3, 2);
+    inv[8] =   M(0, 4)  - M(3, 1);
+#undef M
+
+    det = m[0] * inv[0] + m[1] * inv[3] + m[2] * inv[6];
+
+    if (det == 0) {
+        return false;
+    }
+
+    det = 1.0 / det;
+
+    for (i = 0; i < 9; i++)
+        ((double*)out)[i] = inv[i] * det;
+    return true;
+}
+
+bool mat4_invert(const double mat[S 4][4], double out[S 4][4])
+{
+    double det;
+    int i;
+    const double *m = (const double*)mat;
+    double inv[16];
+
+#define M(i, j, k) m[i] * m[j] * m[k]
+    inv[0] =   M(5, 10, 15) - M(5, 11, 14)  - M(9, 6, 15) +
+               M(9, 7, 14)  + M(13, 6, 11)  - M(13, 7, 10);
+    inv[4] =  -M(4, 10, 15) + M( 4, 11, 14) + M( 8, 6, 15) -
+               M(8, 7, 14)  - M(12,  6, 11) + M(12, 7, 10);
+    inv[8] =   M(4,  9, 15) - M( 4, 11, 13) - M(8,  5, 15) +
+               M(8,  7, 13) + M(12,  5, 11) - M(12, 7, 9);
+    inv[12] = -M(4, 9, 14)  + M(4, 10, 13)  + M(8, 5, 14) -
+               M(8, 6, 13)  - M(12, 5, 10)  + M(12, 6, 9);
+    inv[1] =  -M(1, 10, 15) + M(1, 11, 14)  + M(9, 2, 15) -
+               M(9, 3, 14)  - M(13, 2, 11)  + M(13, 3, 10);
+    inv[5] =   M(0, 10, 15) - M(0, 11, 14)  - M(8, 2, 15) +
+               M(8, 3, 14)  + M(12, 2, 11)  - M(12, 3, 10);
+    inv[9] =  -M(0, 9, 15)  + M(0, 11, 13)  + M(8, 1, 15) -
+               M(8, 3, 13)  - M(12, 1, 11)  + M(12, 3, 9);
+    inv[13] =  M(0, 9, 14)  - M(0, 10, 13)  - M(8, 1, 14) +
+               M(8, 2, 13)  + M(12, 1, 10)  - M(12, 2, 9);
+    inv[2] =   M(1, 6, 15)  - M(1, 7, 14)   - M(5, 2, 15) +
+               M(5, 3, 14)  + M(13, 2, 7)   - M(13, 3, 6);
+    inv[6] =  -M(0, 6, 15)  + M(0, 7, 14)   + M(4, 2, 15) -
+               M(4, 3, 14)  - M(12, 2, 7)   + M(12, 3, 6);
+    inv[10] =  M(0, 5, 15)  - M(0, 7, 13)   - M(4, 1, 15) +
+               M(4, 3, 13)  + M(12, 1, 7)   - M(12, 3, 5);
+    inv[14] = -M(0, 5, 14)  + M(0, 6, 13)   + M(4, 1, 14) -
+               M(4, 2, 13)  - M(12, 1, 6)   + M(12, 2, 5);
+    inv[3] =  -M(1, 6, 11)  + M(1, 7, 10)   + M(5, 2, 11) -
+               M(5, 3, 10)  - M(9, 2, 7)    + M(9, 3, 6);
+    inv[7] =   M(0, 6, 11)  - M(0, 7, 10)   - M(4, 2, 11) +
+               M(4, 3, 10)  + M(8, 2, 7)    - M(8, 3, 6);
+    inv[11] = -M(0, 5, 11)  + M(0, 7, 9)    + M(4, 1, 11) -
+               M(4, 3, 9)   - M(8, 1, 7)    + M(8, 3, 5);
+    inv[15] =  M(0, 5, 10)  - M(0, 6, 9)    - M(4, 1, 10) +
+               M(4, 2, 9)   + M(8, 1, 6)    - M(8, 2, 5);
+#undef M
+
+    det = m[0] * inv[0] + m[1] * inv[4] + m[2] * inv[8] + m[3] * inv[12];
+
+    if (det == 0) {
+        return false;
+    }
+
+    det = 1.0 / det;
+
+    for (i = 0; i < 16; i++)
+        ((double*)out)[i] = inv[i] * det;
+    return true;
+}
+
+
+void mat4_perspective(double mat[S 4][4], double fovy, double aspect,
+                      double nearval, double farval)
+{
+    double radian = fovy * M_PI / 180;
+    double f = 1. / tan(radian / 2.);
+    double ret[4][4] = {
+        {f / aspect, 0., 0., 0.},
+        {0., f, 0., 0.},
+        {0., 0., (farval + nearval) / (nearval - farval), -1},
+        {0., 0., 2. * farval * nearval / (nearval - farval), 0}
+    };
+    mat4_copy(ret, mat);
+}
+
+/*
+ * Perspective projection matrix that puts the far clip at infinity.
+ * Idea from 'Projection Matrix Tricks', by Eric Lengyel.
+ */
+void mat4_inf_perspective(double mat[S 4][4], double fovy, double aspect,
+                          double nearval)
+{
+    double radian = fovy * M_PI / 180;
+    double f = 1. / tan(radian / 2.);
+    const double eps = FLT_EPSILON;
+    double ret[4][4] = {
+        {f / aspect, 0., 0., 0.},
+        {0., f, 0., 0.},
+        {0., 0., eps - 1, -1},
+        {0., 0., (eps - 2) * nearval, 0}
+    };
+    mat4_copy(ret, mat);
+}
+
+void mat4_ortho(double mat[S 4][4], double left, double right,
+                double bottom, double top, double nearval, double farval)
+{
+    double tx = -(right + left) / (right - left);
+    double ty = -(top + bottom) / (top - bottom);
+    double tz = -(farval + nearval) / (farval - nearval);
+    const double ret[4][4] = {
+        {2 / (right - left), 0, 0, 0},
+        {0, 2 / (top - bottom), 0, 0},
+        {0, 0, -2 / (farval - nearval), 0},
+        {tx, ty, tz, 1},
+    };
+    mat4_copy(ret, mat);
+}
+
+
+void quat_slerp(const double a_[S 4], const double b_[S 4], double t,
+                double out[S 4])
+{
+    double a[4] = {a_[0], a_[1], a_[2], a_[3]};
+    double b[4] = {b_[0], b_[1], b_[2], b_[3]};
+    double dot, f1, f2, angle, sin_angle;
+    if (t <= 0.0) {
+        memcpy(out, a, sizeof(a));
+        return;
+    } else if (t >= 1.0) {
+        memcpy(out, b, sizeof(b));
+        return;
+    }
+    dot = a[0] * b[0] + a[1] * b[1] + a[2] * b[2] + a[3] * b[3];
+    if (dot <= 0.0) {
+        quat_ineg(b);
+        dot = -dot;
+    }
+    f1 = 1.0 - t;
+    f2 = t;
+    if ((1.0 - dot) > 0.0000001) {
+        angle = acos(dot);
+        sin_angle = sin(angle);
+        if (sin_angle > 0.0000001) {
+            f1 = sin((1.0 - t) * angle) / sin_angle;
+            f2 = sin(t * angle) / sin_angle;
+        }
+    }
+    out[0] = a[0] * f1 + b[0] * f2;
+    out[1] = a[1] * f1 + b[1] * f2;
+    out[2] = a[2] * f1 + b[2] * f2;
+    out[3] = a[3] * f1 + b[3] * f2;
+}
+
+void cap_great_circle_closest_point(const double cap[S 4],
+                                    const double u[S 3], double out[S 3])
+{
+    double p[3], minusp[3];
+
+    assert(vec3_is_normalized(cap));
+
+    // Look for the point p of the great circle defined by u the closest to cap
+    vec3_cross(u, cap, p);
+    vec3_cross(u, p, p);
+
+    vec3_normalize(p, p);
+    vec3_mul(-1, p, minusp);
+
+    // Pick the correct one from the 2 opposite points
+    if (vec3_dot(cap, p) < vec3_dot(cap, minusp)) {
+        vec3_mul(-1, p, p);
+    }
+
+    vec3_copy(p, out);
+}
+
+bool cap_intersects_segment(const double cap[S 4], const double p0[S 3],
+                            const double p1[S 3])
+{
+    // Construct the vec3 u, orthogonal to the great circle containing p0 and p1
+    double u[3], k[3], p[3];
+    double c;
+
+    vec3_cross(p0, p1, u);
+
+    // Deal with the case where the cap and u are co-linear
+    c = vec3_dot(cap, u);
+    if (c*c >= vec3_norm2(u))
+        return cap[3] <= 0;
+
+    cap_great_circle_closest_point(cap, u, p);
+
+    // If the closest point is not in the cap there is no intersection
+    if (!cap_contains_vec3(cap, p))
+        return false;
+
+    // Construct the cap with p0 and p1 on its edge
+    double cap_geo[4];
+    vec3_add(p0, p1, k);
+    vec3_normalize(k, cap_geo);
+    cap_geo[3] = vec3_dot(cap_geo, p1);
+
+    // If the closest point is in the cap and within the segment boundaries
+    // we know that they intersect.
+    if (cap_contains_vec3(cap_geo, p))
+        return true;
+
+    return cap_contains_vec3(cap, p0) || cap_contains_vec3(cap, p1);
+}
+
 
 /******** TESTS ***********************************************************/
 
--- src/utils/vec.h
+++ src/utils/vec.h
@@ -145,7 +145,7 @@ DEF void mat3_ry(double a, const double mat[S 3][3], double out[S 3][3]);
 DEF void mat3_rz(double a, const double mat[S 3][3], double out[S 3][3]);
 DEF void mat3_itranslate(double m[S 3][3], double x, double y);
 DEF void mat3_iscale(double m[S 3][3], double x, double y, double z);
-DEF bool mat3_invert(const double mat[S 3][3], double out[S 3][3]);
+bool mat3_invert(const double mat[S 3][3], double out[S 3][3]);
 DEF void mat3_transpose(const double mat[S 3][3], double out[S 3][3]);
 DEF void mat3_to_mat4(const double mat[S 3][3], double out[S 4][4]);
 DEF void mat3_to_float(const double mat[S 3][3], float out[S 9]);
@@ -159,12 +159,19 @@ DEF void mat4_mul_vec3(const double mat[S 4][4], const double v[S 3],
 DEF void mat4_mul_dir3(const double mat[S 4][4], const double v[S 3],
                        double out[S 3]);
 DEF void mat4_copy(const double src[S 4][4], double out[S 4][4]);
-DEF void mat4_perspective(double mat[S 4][4], double fovy, double aspect,
+
+void mat4_perspective(double mat[S 4][4], double fovy, double aspect,
                           double nearval, double farval);
-DEF void mat4_inf_perspective(double mat[S 4][4], double fovy, double aspect,
-                              double nearval);
-DEF void mat4_ortho(double mat[S 4][4], double left, double right,
-                    double bottom, double top, double nearval, double farval);
+/*
+ * Perspective projection matrix that puts the far clip at infinity.
+ * Idea from 'Projection Matrix Tricks', by Eric Lengyel.
+ */
+void mat4_inf_perspective(double mat[S 4][4], double fovy, double aspect,
+                          double nearval);
+
+void mat4_ortho(double mat[S 4][4], double left, double right,
+                double bottom, double top, double nearval, double farval);
+
 DEF void mat4_to_float(const double mat[S 4][4], float out[S 16]);
 DEF void mat4_set_identity(double mat[S 4][4]);
 DEF bool mat4_is_identity(const double mat[S 4][4]);
@@ -178,7 +185,7 @@ DEF void mat4_ry(double a, const double mat[S 4][4], double out[S 4][4]);
 DEF void mat4_rz(double a, const double mat[S 4][4], double out[S 4][4]);
 DEF void mat4_itranslate(double m[S 4][4], double x, double y, double z);
 DEF void mat4_iscale(double m[S 4][4], double x, double y, double z);
-DEF bool mat4_invert(const double mat[S 4][4], double out[S 4][4]);
+bool mat4_invert(const double mat[S 4][4], double out[S 4][4]);
 DEF void mat4_transpose(const double mat[S 4][4], double out[S 4][4]);
 
 DEF void quat_set_identity(double q[S 4]);
@@ -190,8 +197,8 @@ DEF void quat_mul(const double a[S 4], const double b[S 4], double out[S 4]);
 DEF void quat_mul_vec3(const double q[S 4], const double v[S 3],
                        double out[S 3]);
 DEF void quat_to_mat3(const double q[S 4], double out[S 3][3]);
-DEF void quat_slerp(const double a[S 4], const double b[S 4], double t,
-                    double out[S 4]);
+void quat_slerp(const double a[S 4], const double b[S 4], double t,
+                double out[S 4]);
 // Separation angle between two quaternions.
 double quat_sep(const double a[S 4], const double b[S 4]);
 void quat_rotate_towards(const double a[S 4], const double b[S 4],
@@ -201,6 +208,10 @@ DEF void quat_normalize(const double q[S 4], double out[S 4]);
 DEF bool cap_contains_vec3(const double cap[S 4], const double v[S 3]);
 DEF bool cap_contains_cap(const double cap[S 4], const double c[S 4]);
 DEF bool cap_intersects_cap(const double cap[S 4], const double c[S 4]);
+bool cap_intersects_segment(const double cap[S 4], const double p0[S 3],
+                            const double p1[S 3]);
+void cap_great_circle_closest_point(const double cap[S 4],
+                                    const double u[S 3], double out[S 3]);
 
 #endif
 
@@ -532,54 +543,6 @@ DEF void mat4_copy(const double src[S 4][4], double out[S 4][4])
     memcpy(out, src, 4 * 4 * sizeof(src[0][0]));
 }
 
-DEF void mat4_perspective(double mat[S 4][4], double fovy, double aspect,
-                          double nearval, double farval)
-{
-    double radian = fovy * M_PI / 180;
-    double f = 1. / tan(radian / 2.);
-    double ret[4][4] = {
-        {f / aspect, 0., 0., 0.},
-        {0., f, 0., 0.},
-        {0., 0., (farval + nearval) / (nearval - farval), -1},
-        {0., 0., 2. * farval * nearval / (nearval - farval), 0}
-    };
-    mat4_copy(ret, mat);
-}
-
-/*
- * Perspective projection matrix that puts the far clip at infinity.
- * Idea from 'Projection Matrix Tricks', by Eric Lengyel.
- */
-DEF void mat4_inf_perspective(double mat[S 4][4], double fovy, double aspect,
-                              double nearval)
-{
-    double radian = fovy * M_PI / 180;
-    double f = 1. / tan(radian / 2.);
-    const double eps = FLT_EPSILON;
-    double ret[4][4] = {
-        {f / aspect, 0., 0., 0.},
-        {0., f, 0., 0.},
-        {0., 0., eps - 1, -1},
-        {0., 0., (eps - 2) * nearval, 0}
-    };
-    mat4_copy(ret, mat);
-}
-
-DEF void mat4_ortho(double mat[S 4][4], double left, double right,
-                    double bottom, double top, double nearval, double farval)
-{
-    double tx = -(right + left) / (right - left);
-    double ty = -(top + bottom) / (top - bottom);
-    double tz = -(farval + nearval) / (farval - nearval);
-    const double ret[4][4] = {
-        {2 / (right - left), 0, 0, 0},
-        {0, 2 / (top - bottom), 0, 0},
-        {0, 0, -2 / (farval - nearval), 0},
-        {tx, ty, tz, 1},
-    };
-    mat4_copy(ret, mat);
-}
-
 DEF void mat3_to_float(const double mat[S 3][3], float out[S 9])
 {
     int i, j;
@@ -771,95 +734,6 @@ DEF void mat4_iscale(double m[S 4][4], double x, double y, double z)
     }
 }
 
-DEF bool mat3_invert(const double mat[S 3][3], double out[S 3][3])
-{
-    double det;
-    int i;
-    const double *m = (const double *)mat;
-    double inv[9];
-
-#define M(i, j) m[i] * m[j]
-    inv[0] =   M(4, 8) - M(7, 5);
-    inv[3] =  -M(3, 8) + M(6, 5);
-    inv[6] =   M(3, 7) - M(6, 4);
-
-    inv[1] =  -M(1, 8) + M(7, 2);
-    inv[4] =   M(0, 8) - M(6, 2);
-    inv[7] =  -M(0, 7) + M(6, 1);
-
-    inv[2] =   M(1, 5)  - M(4, 2);
-    inv[5] =  -M(0, 5)  + M(3, 2);
-    inv[8] =   M(0, 4)  - M(3, 1);
-#undef M
-
-    det = m[0] * inv[0] + m[1] * inv[3] + m[2] * inv[6];
-
-    if (det == 0) {
-        return false;
-    }
-
-    det = 1.0 / det;
-
-    for (i = 0; i < 9; i++)
-        ((double*)out)[i] = inv[i] * det;
-    return true;
-}
-
-DEF bool mat4_invert(const double mat[S 4][4], double out[S 4][4])
-{
-    double det;
-    int i;
-    const double *m = (const double*)mat;
-    double inv[16];
-
-#define M(i, j, k) m[i] * m[j] * m[k]
-    inv[0] =   M(5, 10, 15) - M(5, 11, 14)  - M(9, 6, 15) +
-               M(9, 7, 14)  + M(13, 6, 11)  - M(13, 7, 10);
-    inv[4] =  -M(4, 10, 15) + M( 4, 11, 14) + M( 8, 6, 15) -
-               M(8, 7, 14)  - M(12,  6, 11) + M(12, 7, 10);
-    inv[8] =   M(4,  9, 15) - M( 4, 11, 13) - M(8,  5, 15) +
-               M(8,  7, 13) + M(12,  5, 11) - M(12, 7, 9);
-    inv[12] = -M(4, 9, 14)  + M(4, 10, 13)  + M(8, 5, 14) -
-               M(8, 6, 13)  - M(12, 5, 10)  + M(12, 6, 9);
-    inv[1] =  -M(1, 10, 15) + M(1, 11, 14)  + M(9, 2, 15) -
-               M(9, 3, 14)  - M(13, 2, 11)  + M(13, 3, 10);
-    inv[5] =   M(0, 10, 15) - M(0, 11, 14)  - M(8, 2, 15) +
-               M(8, 3, 14)  + M(12, 2, 11)  - M(12, 3, 10);
-    inv[9] =  -M(0, 9, 15)  + M(0, 11, 13)  + M(8, 1, 15) -
-               M(8, 3, 13)  - M(12, 1, 11)  + M(12, 3, 9);
-    inv[13] =  M(0, 9, 14)  - M(0, 10, 13)  - M(8, 1, 14) +
-               M(8, 2, 13)  + M(12, 1, 10)  - M(12, 2, 9);
-    inv[2] =   M(1, 6, 15)  - M(1, 7, 14)   - M(5, 2, 15) +
-               M(5, 3, 14)  + M(13, 2, 7)   - M(13, 3, 6);
-    inv[6] =  -M(0, 6, 15)  + M(0, 7, 14)   + M(4, 2, 15) -
-               M(4, 3, 14)  - M(12, 2, 7)   + M(12, 3, 6);
-    inv[10] =  M(0, 5, 15)  - M(0, 7, 13)   - M(4, 1, 15) +
-               M(4, 3, 13)  + M(12, 1, 7)   - M(12, 3, 5);
-    inv[14] = -M(0, 5, 14)  + M(0, 6, 13)   + M(4, 1, 14) -
-               M(4, 2, 13)  - M(12, 1, 6)   + M(12, 2, 5);
-    inv[3] =  -M(1, 6, 11)  + M(1, 7, 10)   + M(5, 2, 11) -
-               M(5, 3, 10)  - M(9, 2, 7)    + M(9, 3, 6);
-    inv[7] =   M(0, 6, 11)  - M(0, 7, 10)   - M(4, 2, 11) +
-               M(4, 3, 10)  + M(8, 2, 7)    - M(8, 3, 6);
-    inv[11] = -M(0, 5, 11)  + M(0, 7, 9)    + M(4, 1, 11) -
-               M(4, 3, 9)   - M(8, 1, 7)    + M(8, 3, 5);
-    inv[15] =  M(0, 5, 10)  - M(0, 6, 9)    - M(4, 1, 10) +
-               M(4, 2, 9)   + M(8, 1, 6)    - M(8, 2, 5);
-#undef M
-
-    det = m[0] * inv[0] + m[1] * inv[4] + m[2] * inv[8] + m[3] * inv[12];
-
-    if (det == 0) {
-        return false;
-    }
-
-    det = 1.0 / det;
-
-    for (i = 0; i < 16; i++)
-        ((double*)out)[i] = inv[i] * det;
-    return true;
-}
-
 DEF void mat3_transpose(const double mat[S 3][3], double out[S 3][3])
 {
     double tmp[3][3];
@@ -986,40 +860,6 @@ DEF void quat_rz(double a, const double q[S 4], double out[S 4])
     quat_mul(q, tmp, out);
 }
 
-DEF void quat_slerp(const double a_[S 4], const double b_[S 4], double t,
-                    double out[S 4])
-{
-    double a[4] = {a_[0], a_[1], a_[2], a_[3]};
-    double b[4] = {b_[0], b_[1], b_[2], b_[3]};
-    double dot, f1, f2, angle, sin_angle;
-    if (t <= 0.0) {
-        memcpy(out, a, sizeof(a));
-        return;
-    } else if (t >= 1.0) {
-        memcpy(out, b, sizeof(b));
-        return;
-    }
-    dot = a[0] * b[0] + a[1] * b[1] + a[2] * b[2] + a[3] * b[3];
-    if (dot <= 0.0) {
-        quat_ineg(b);
-        dot = -dot;
-    }
-    f1 = 1.0 - t;
-    f2 = t;
-    if ((1.0 - dot) > 0.0000001) {
-        angle = acos(dot);
-        sin_angle = sin(angle);
-        if (sin_angle > 0.0000001) {
-            f1 = sin((1.0 - t) * angle) / sin_angle;
-            f2 = sin(t * angle) / sin_angle;
-        }
-    }
-    out[0] = a[0] * f1 + b[0] * f2;
-    out[1] = a[1] * f1 + b[1] * f2;
-    out[2] = a[2] * f1 + b[2] * f2;
-    out[3] = a[3] * f1 + b[3] * f2;
-}
-
 DEF void quat_normalize(const double q[S 4], double out[S 4])
 {
     double n = sqrt(q[0] * q[0] + q[1] * q[1] + q[2] * q[2] + q[3] * q[3]);
@@ -1056,61 +896,6 @@ DEF bool cap_intersects_cap(const double cap[S 4], const double c[S 4])
       (a <= 1.0 && a * a <= (1.0 - d1 * d1) * (1.0 - d2 * d2));
 }
 
-DEF void cap_great_circle_closest_point(const double cap[S 4],
-                                        const double u[S 3], double out[S 3])
-{
-    double p[3], minusp[3];
-
-    assert(vec3_is_normalized(cap));
-
-    // Look for the point p of the great circle defined by u the closest to cap
-    vec3_cross(u, cap, p);
-    vec3_cross(u, p, p);
-
-    vec3_normalize(p, p);
-    vec3_mul(-1, p, minusp);
-
-    // Pick the correct one from the 2 opposite points
-    if (vec3_dot(cap, p) < vec3_dot(cap, minusp)) {
-        vec3_mul(-1, p, p);
-    }
-
-    vec3_copy(p, out);
-}
-
-DEF bool cap_intersects_segment(const double cap[S 4], const double p0[S 3],
-                                const double p1[S 3])
-{
-    // Construct the vec3 u, orthogonal to the great circle containing p0 and p1
-    double u[3], k[3], p[3];
-    double c;
-
-    vec3_cross(p0, p1, u);
-
-    // Deal with the case where the cap and u are co-linear
-    c = vec3_dot(cap, u);
-    if (c*c >= vec3_norm2(u))
-        return cap[3] <= 0;
-
-    cap_great_circle_closest_point(cap, u, p);
-
-    // If the closest point is not in the cap there is no intersection
-    if (!cap_contains_vec3(cap, p))
-        return false;
-
-    // Construct the cap with p0 and p1 on its edge
-    double cap_geo[4];
-    vec3_add(p0, p1, k);
-    vec3_normalize(k, cap_geo);
-    cap_geo[3] = vec3_dot(cap_geo, p1);
-
-    // If the closest point is in the cap and within the segment boundaries
-    // we know that they intersect.
-    if (cap_contains_vec3(cap_geo, p))
-        return true;
-
-    return cap_contains_vec3(cap, p0) || cap_contains_vec3(cap, p1);
-}
 
 #endif
 
