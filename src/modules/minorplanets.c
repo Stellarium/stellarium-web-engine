53a1bac1c7949411d76bdedca7fdc02e1f5d934e
Guillaume Chereau <guillaume@noctua-software.com>
2021-05-14 12:42:33 +0800

    Fix 3d model rendering of minor planets
    Need to enable the depth testing, and set the depth range.  Note that I
    would still like to remove the depth range and automatically compute it
    instead.

--- src/modules/minorplanets.c
+++ src/modules/minorplanets.c
@@ -266,12 +266,22 @@ static int mplanet_get_info(const obj_t *obj, const observer_t *obs, int info,
     return 1;
 }
 
-static int render_3d_model(const mplanet_t *mplanet, const painter_t *painter)
+static int render_3d_model(const mplanet_t *mplanet, const painter_t *painter_)
 {
+    painter_t painter = *painter_;
     double model_mat[4][4] = MAT4_IDENTITY;
+
+    painter.flags |= PAINTER_ENABLE_DEPTH;
     mat4_itranslate(model_mat, VEC3_SPLIT(mplanet->pvo[0]));
+
+    double dist, depth_range[2];
+    dist = vec3_norm(mplanet->pvo[0]);
+    depth_range[0] = dist * 0.5;
+    depth_range[1] = dist * 2;
+    painter.depth_range = &depth_range;
+
     mat4_iscale(model_mat, 1000 * DM2AU, 1000 * DM2AU, 1000 * DM2AU);
-    paint_3d_model(painter, mplanet->model, model_mat, NULL);
+    paint_3d_model(&painter, mplanet->model, model_mat, NULL);
     return 0;
 }
 
