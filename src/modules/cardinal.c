7b2209b5f95b368a7f92ebec4ae7c46d3be6f56f
Fabien Chereau <fabien.chereau@gmail.com>
2021-10-27 16:20:55 +0400

    Change the cardinal points rendering

--- src/modules/cardinal.c
+++ src/modules/cardinal.c
@@ -55,15 +55,26 @@ static int cardinal_render(obj_t *obj, const painter_t *painter)
     double size = 24;
     const cardinal_t *c = (const cardinal_t*)obj;
     double color[4] = {0.8, 0.4, 0.4, 0.8 * c->visible.value};
+    double p[4];
 
     if (c->visible.value <= 0) return 0;
     for (i = 0; i < 4; i++) {
         if (painter_is_point_clipped_fast(painter, FRAME_OBSERVED,
                 POINTS[i].pos, true))
             continue;
+        convert_frame(painter->obs, FRAME_OBSERVED, FRAME_VIEW, true,
+                      POINTS[i].pos, p);
+        project_to_win(painter->proj, p, p);
+        painter_t _painter = *painter;
+        _painter.color[0] = color[0];
+        _painter.color[1] = color[1];
+        _painter.color[2] = color[2];
+        _painter.color[3] = c->visible.value;
+        _painter.lines.width = 4;
+        paint_2d_ellipse(&_painter, NULL, 0, p, VEC(1, 1), NULL);
         labels_add_3d(sys_translate("gui", POINTS[i].text), FRAME_OBSERVED,
                       POINTS[i].pos, true, 0, size, color, 0,
-                      ALIGN_CENTER | ALIGN_MIDDLE, TEXT_BOLD, 0, NULL);
+                      ALIGN_CENTER | ALIGN_TOP, TEXT_BOLD, 0, NULL);
     }
     return 0;
 }
