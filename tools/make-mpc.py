6ee2a2e71ca3639198875c6f623fd014c7ae3596
Guillaume Chereau <guillaume@noctua-software.com>
2021-12-08 14:03:12 +0800

    Update make-mpc.py tool
    - Don't use the 'download' helper: it makes the code hard to follow just
      to save a few lines.
    
    - Fix the output directory.

--- tools/make-mpc.py
+++ tools/make-mpc.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-# coding: utf-8
 
 # Stellarium Web Engine - Copyright (c) 2018 - Noctua Software Ltd
 #
@@ -9,14 +8,19 @@
 # The terms of the AGPL v3 license can be found in the main directory of this
 # repository.
 
+import os
 import gzip
-
-from utils import download
+import requests
 
 URL = 'https://minorplanetcenter.net/Extended_Files/mpcorb_extended.dat.gz'
 
 def run():
-    path = download(URL)
+    path = '/tmp/mpcorb_extended.dat.gz'
+    if not os.path.exists(path):
+        print(f'download {URL}')
+        r = requests.get(URL)
+        with open(path, 'wb') as out:
+            out.write(r.content)
     lines = gzip.open(path, 'rt').readlines()
     lines = [x for x in lines if len(x) > 162]
     # Remove Pluto (we have it as a planet)
@@ -25,7 +29,7 @@ def run():
     lines = sorted(lines, key=lambda x: float(x[8:14].strip() or 'inf'))
     # Keep only 500 first.
     lines = lines[:500]
-    out = open("data/skydata/mpcorb.dat", "w")
+    out = open("apps/test-skydata/mpcorb.dat", "w")
     for line in lines:
         print(line, file=out)
     out.close()
